5445e8dc7d43a4304fef96bd5f573f59c6109c04
Jubergie <julie-2.bergeron@polymtl.ca>
2024-01-15 13:33:43 -0500

    [feature] completed initView

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -3,12 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
+import { NewGamePageComponent } from '@app/pages/new-game-page/new-game-page.component';
+
+
+
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
+    { path: 'admin', component: AdminPageComponent },
+    { path: 'new-game', component: NewGamePageComponent },
+
     { path: '**', redirectTo: '/home' },
 ];
 
