18841f9b5fe05c588a66480c95c74076a28acfa7
Jubergie <julie-2.bergeron@polymtl.ca>
2024-01-16 13:55:52 -0500

    [Page] create-qgame

---
+++ client/src/app/pages/create-qgame-page/create-qgame-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-qgame-page',
+  templateUrl: './create-qgame-page.component.html',
+  styleUrls: ['./create-qgame-page.component.scss']
+})
+export class CreateQGamePageComponent {
+
+}
