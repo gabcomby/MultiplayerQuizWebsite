952c13b8f46e0a467969089b55ef3040524ae2ac
chapsag <goffi.pro@icloud.com>
2024-01-17 12:53:53 -0500

    [FEATURE] Update Game interface and AdminPageComponent

--- client/src/app/interfaces/game.ts
+++ client/src/app/interfaces/game.ts
@@ -1,7 +1,21 @@
 export interface Game {
-    id: number;
+    id: string;
     title: string;
     description: string;
     isVisible: boolean;
-    lastUpdate: Date;
+    duration: number;
+    lastModification: Date;
+    questions: Question[];
+}
+
+interface Choice {
+    text: string;
+    isCorrect?: boolean;
+}
+
+interface Question {
+    type: string;
+    text: string;
+    points: number;
+    choices?: Choice[];
 }
--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -1,30 +1,34 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { Game } from '@app/interfaces/game';
-
-const FAKE_GAMES: Game[] = [
-    { id: 1, title: 'Party 1', description: 'Description of Party 1', isVisible: true, lastUpdate: new Date() },
-    { id: 2, title: 'Party 2', description: 'Description of Party 2', isVisible: true, lastUpdate: new Date() },
-    { id: 3, title: 'Party 3', description: 'Description of Party 3', isVisible: true, lastUpdate: new Date() },
-    { id: 4, title: 'Party 4', description: 'Description of Party 4', isVisible: true, lastUpdate: new Date() },
-    { id: 5, title: 'Party 5', description: 'Description of Party 5', isVisible: true, lastUpdate: new Date() },
-    { id: 6, title: 'Party 6', description: 'Description of Party 6', isVisible: true, lastUpdate: new Date() },
-    { id: 7, title: 'Party 7', description: 'Description of Party 7', isVisible: true, lastUpdate: new Date() },
-    { id: 8, title: 'Party 8', description: 'Description of Party 8', isVisible: true, lastUpdate: new Date() },
-    { id: 9, title: 'Party 9', description: 'Description of Party 9', isVisible: true, lastUpdate: new Date() },
-    { id: 10, title: 'Party 10', description: 'Description of Party 10', isVisible: true, lastUpdate: new Date() },
-];
 
 @Component({
     selector: 'app-admin-page',
     templateUrl: './admin-page.component.html',
     styleUrls: ['./admin-page.component.scss'],
 })
-export class AdminPageComponent {
+export class AdminPageComponent implements OnInit {
     displayedColumns: string[] = ['id', 'title', 'isVisible', 'lastUpdate', 'export', 'delete'];
-    dataSource = FAKE_GAMES;
+    dataSource: Game[] = [];
 
-    toggleVisibility(gameId: number, isVisible: boolean): void {
+    constructor(private http: HttpClient) {}
+
+    ngOnInit() {
+        this.loadGames();
+    }
+
+    loadGames(): void {
+        this.http.get<Game[]>('http://localhost:3000/api/games').subscribe({
+            next: (data) => {
+                this.dataSource = data;
+            },
+            error: (error) => {
+                alert(error);
+            },
+        });
+    }
+
+    toggleVisibility(gameId: string, isVisible: boolean): void {
         const game = this.dataSource.find((g) => g.id === gameId);
         if (game) {
             game.isVisible = isVisible;
@@ -32,7 +36,7 @@ export class AdminPageComponent {
     }
 
     exportGameAsJson(game: Game): void {
-        const exportData = { id: game.id, title: game.title, description: game.description, lastUpdate: game.lastUpdate };
+        const exportData = { id: game.id, title: game.title, description: game.description, lastUpdate: game.lastModification };
 
         const dataStr = JSON.stringify(exportData);
         const dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(dataStr);
@@ -74,7 +78,7 @@ export class AdminPageComponent {
         reader.readAsText(file);
     }
 
-    deleteGame(gameId: number): void {
+    deleteGame(gameId: string): void {
         this.dataSource = this.dataSource.filter((game) => game.id !== gameId);
     }
 
