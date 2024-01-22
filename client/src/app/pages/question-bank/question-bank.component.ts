0f17c5dbd0267c0e5c8e0ca8d4adf56f57a50db1
Maxsavy <maxime.savy@polymtl.ca>
2024-01-22 16:04:20 -0500

    [feature] creation of the question bank component

---
+++ client/src/app/pages/question-bank/question-bank.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-question-bank',
+    standalone: true,
+    imports: [],
+    templateUrl: './question-bank.component.html',
+    styleUrl: './question-bank.component.scss',
+})
+export class QuestionBankComponent {}
