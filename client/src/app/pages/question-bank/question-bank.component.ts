ceda114c4bf06752ed093b735e2c0d047708adaf
Maxsavy <maxime.savy@polymtl.ca>
2024-01-22 17:12:53 -0500

    [feature] new angular table for the questions

--- client/src/app/pages/question-bank/question-bank.component.ts
+++ client/src/app/pages/question-bank/question-bank.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+export interface QuestionBank {
+    position: number;
+    question: string;
+    date: Date;
+}
+
+const ELEMENT_DATA: QuestionBank[] = [
+    { position: 1, question: 'Question 1', date: new Date() },
+    { position: 2, question: 'Question 2', date: new Date() },
+    { position: 3, question: 'Question 3', date: new Date() },
+    { position: 4, question: 'Question 4', date: new Date() },
+];
 
 @Component({
     selector: 'app-question-bank',
     standalone: true,
-    imports: [],
+    imports: [MatTableModule],
     templateUrl: './question-bank.component.html',
     styleUrl: './question-bank.component.scss',
 })
-export class QuestionBankComponent {}
+export class QuestionBankComponent {
+    displayedColumns: string[] = ['position', 'question', 'date'];
+    dataSource = ELEMENT_DATA;
+    clickedRows = new Set<QuestionBank>();
+}
