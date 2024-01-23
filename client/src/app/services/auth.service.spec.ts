2434cdc1ac3ed0feb35f0833dfc4d6793545f6a9
chapsag <goffi.pro@icloud.com>
2024-01-23 10:01:31 -0500

    [TEST] Add HttpClientTestingModule and HttpTestingController to AuthService spec

--- client/src/app/services/auth.service.spec.ts
+++ client/src/app/services/auth.service.spec.ts
@@ -1,16 +1,38 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
     let service: AuthService;
+    let httpController: HttpTestingController;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [AuthService],
+        });
         service = TestBed.inject(AuthService);
+        httpController = TestBed.inject(HttpTestingController);
+    });
+
+    afterEach(() => {
+        httpController.verify();
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should return expected result when authenticated', () => {
+        const testPassword = 'password123';
+        const testResponse = true;
+
+        service.authenticate(testPassword).subscribe((response) => {
+            expect(response).toEqual(testResponse);
+        });
+
+        const req = httpController.expectOne('http://localhost:3000/api/authenticate');
+        expect(req.request.method).toEqual('POST');
+        req.flush(testResponse);
     });
 });
