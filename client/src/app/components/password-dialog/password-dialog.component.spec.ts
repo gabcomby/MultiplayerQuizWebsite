e5eaa083bed9709edfe582c07c2a2228be815f11
chapsag <goffi.pro@icloud.com>
2024-01-23 11:05:48 -0500

    [TEST] Add mock for MatDialogRef in PasswordDialogComponent spec

--- client/src/app/components/password-dialog/password-dialog.component.spec.ts
+++ client/src/app/components/password-dialog/password-dialog.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef } from '@angular/material/dialog';
 
 import { PasswordDialogComponent } from './password-dialog.component';
 
@@ -6,10 +7,17 @@ describe('PasswordDialogComponent', () => {
     let component: PasswordDialogComponent;
     let fixture: ComponentFixture<PasswordDialogComponent>;
 
+    // Mock the MatDialogRef
+    const dialogRefMock = {
+        close: jasmine.createSpy('close'),
+    };
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [PasswordDialogComponent],
+            providers: [{ provide: MatDialogRef, useValue: dialogRefMock }],
         });
+
         fixture = TestBed.createComponent(PasswordDialogComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -17,5 +25,10 @@ describe('PasswordDialogComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should close the dialog on calling onNoClick()', () => {
+        component.onNoClick();
+        expect(dialogRefMock.close).toHaveBeenCalled();
     });
 });
