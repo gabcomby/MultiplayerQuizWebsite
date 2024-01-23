562c35699e744050a323e108172c8e95fc4dd222
Gabriel Comby <combygabriel@gmail.com>
2024-01-23 10:18:00 -0500

    [FEATURE] Added a way to get messages sent in chat in game view

--- client/src/app/components/game-page-livechat/game-page-livechat.component.ts
+++ client/src/app/components/game-page-livechat/game-page-livechat.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
     templateUrl: './game-page-livechat.component.html',
     styleUrls: ['./game-page-livechat.component.scss'],
 })
-export class GamePageLivechatComponent {}
+export class GamePageLivechatComponent {
+    messages: string[] = [];
+    newMessage: string = '';
+
+    sendMessage(): void {
+        if (this.newMessage.trim() !== '') {
+            this.messages.push(this.newMessage);
+            alert(`Message sent: ${this.newMessage}`);
+            this.newMessage = '';
+        } else {
+            this.newMessage = '';
+        }
+    }
+}
