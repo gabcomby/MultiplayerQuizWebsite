fb27e68c3792bc9f61bbd3b76f7859d02a53cd43
chapsag <goffi.pro@icloud.com>
2024-01-23 11:19:30 -0500

    [BUGFIX] Enable MatInputModule in material.module.ts

--- client/src/app/modules/material.module.ts
+++ client/src/app/modules/material.module.ts
@@ -11,7 +11,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 // import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
-// import { MatInputModule } from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 // import { MatListModule } from '@angular/material/list';
 // import { MatMenuModule } from '@angular/material/menu';
 // import { MatPaginatorModule } from '@angular/material/paginator';
@@ -44,7 +44,7 @@ const modules = [
     MatFormFieldModule,
     // MatGridListModule,
     MatIconModule,
-    // MatInputModule,
+    MatInputModule,
     // MatListModule,
     // MatMenuModule,
     // MatPaginatorModule,
