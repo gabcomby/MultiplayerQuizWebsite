279ec0966036817c4d6d9b81a5a4b6ae0f363f91
Gabriel Comby <combygabriel@gmail.com>
2024-01-24 18:15:17 -0500

    [BUGFIX] Fixed non-working imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
-import { GamePageLivechatComponent } from './components/game-page-livechat/game-page-livechat.component';
 import { AppRoutingModule } from '@app/modules/app-routing.module';
 import { AppMaterialModule } from '@app/modules/material.module';
 import { AppComponent } from '@app/pages/app/app.component';
@@ -12,17 +11,18 @@ import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { ChoiceComponent } from './components/choice/choice.component';
-import { AdminPageComponent } from './pages/admin-page/admin-page.component';
-import { NewGamePageComponent } from './pages/new-game-page/new-game-page.component';
-import { NewQuestionComponent } from './components/new-question/new-question.component';
-import { CreateQGamePageComponent } from './pages/create-qgame-page/create-qgame-page.component';
+import { GamePageLivechatComponent } from './components/game-page-livechat/game-page-livechat.component';
 import { GamePageQuestionsComponent } from './components/game-page-questions/game-page-questions.component';
-import { GamePageTimerComponent } from './components/game-page-timer/game-page-timer.component';
 import { GamePageScoresheetComponent } from './components/game-page-scoresheet/game-page-scoresheet.component';
+import { GamePageTimerComponent } from './components/game-page-timer/game-page-timer.component';
+import { NewQuestionComponent } from './components/new-question/new-question.component';
+import { PasswordDialogComponent } from './components/password-dialog/password-dialog.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { CreateQGamePageComponent } from './pages/create-qgame-page/create-qgame-page.component';
 import { GameTestComponent } from './pages/game-test/game-test.component';
 import { GameWaitComponent } from './pages/game-wait/game-wait.component';
+import { NewGamePageComponent } from './pages/new-game-page/new-game-page.component';
 import { QuestionsPageComponent } from './pages/questions-page/questions-page.component';
-import { PasswordDialogComponent } from './components/password-dialog/password-dialog.component';
 
 /**
  * Main module that is used in main.ts.
@@ -44,6 +44,13 @@ import { PasswordDialogComponent } from './components/password-dialog/password-d
         NewQuestionComponent,
         ChoiceComponent,
         CreateQGamePageComponent,
+        GamePageQuestionsComponent,
+        GamePageTimerComponent,
+        GamePageScoresheetComponent,
+        GameTestComponent,
+        GameWaitComponent,
+        QuestionsPageComponent,
+        PasswordDialogComponent,
     ],
     imports: [
         AppMaterialModule,
@@ -54,13 +61,6 @@ import { PasswordDialogComponent } from './components/password-dialog/password-d
         HttpClientModule,
         ReactiveFormsModule,
         FormsModule,
-        GamePageQuestionsComponent,
-        GamePageTimerComponent,
-        GamePageScoresheetComponent,
-        GameTestComponent,
-        GameWaitComponent,
-        QuestionsPageComponent,
-        PasswordDialogComponent,
     ],
     providers: [],
     bootstrap: [AppComponent],
