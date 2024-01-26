fb3560a0932cd687ee3ab5d62d168fbeacbb0aa9
chapsag <goffi.pro@icloud.com>
2024-01-26 12:28:57 -0500

    [BUGFIX] Fix linting

--- client/src/app/services/timer.service.spec.ts
+++ client/src/app/services/timer.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { TimerService } from './timer.service';
 
 describe('TimerService', () => {
-  let service: TimerService;
+    let service: TimerService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TimerService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(TimerService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
