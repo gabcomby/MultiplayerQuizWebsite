5509e9e9ffe695c8ef0fc8b0e2f2e6c1e395ec74
judaoc <112410161+judaoc@users.noreply.github.com>
2024-01-27 15:06:50 -0500

    [FEATURE] connected to mongoDB

--- client/src/app/pages/new-game-page/new-game-page.component.ts
+++ client/src/app/pages/new-game-page/new-game-page.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { games, Game } from '@app/pages/game';
+import { Component, OnInit } from '@angular/core';
+import { GameService } from '@app/services/game.service';
+import { Game } from '@app/interfaces/game';
 
 @Component({
     selector: 'app-new-game-page',
     templateUrl: './new-game-page.component.html',
     styleUrls: ['./new-game-page.component.scss'],
 })
-export class NewGamePageComponent {
-    games = [...games];
+export class NewGamePageComponent implements OnInit {
+    games: Game[] = [];
     gameSelected: { [key: string]: boolean } = {};
+    constructor(private gameService: GameService) {}
+    get arrayGames(): Game[] {
+        return this.games;
+    }
+    ngOnInit() {
+        this.gameService.getGames().then((games) => {
+            this.games = games;
+        });
+    }
     getInformations(game: Game) {
         this.gameSelected[game.id] = !this.gameSelected[game.id];
     }
