ccd08ccd530f2c43f8173bc6f2cbfac41d3b5c2b
judaoc <112410161+judaoc@users.noreply.github.com>
2024-01-28 14:05:42 -0500

    [BUGFIX] all the basic tests runs

--- client/src/app/pages/new-game-page/new-game-page.component.spec.ts
+++ client/src/app/pages/new-game-page/new-game-page.component.spec.ts
@@ -1,15 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { GameService } from '@app/services/game.service';
 import { NewGamePageComponent } from './new-game-page.component';
 
 describe('NewGamePageComponent', () => {
     let component: NewGamePageComponent;
     let fixture: ComponentFixture<NewGamePageComponent>;
-
-    beforeEach(() => {
-        TestBed.configureTestingModule({
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
             declarations: [NewGamePageComponent],
-        });
+            providers: [GameService],
+            imports: [HttpClientModule],
+
+        }).compileComponents();
         fixture = TestBed.createComponent(NewGamePageComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
