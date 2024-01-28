822dac9b3ec9324e385a4b99e037518dc1a3df46
judaoc <112410161+judaoc@users.noreply.github.com>
2024-01-28 17:59:48 -0500

    [FEATURE] tests are done for the new game page typeScript

--- client/src/app/pages/new-game-page/new-game-page.component.spec.ts
+++ client/src/app/pages/new-game-page/new-game-page.component.spec.ts
@@ -24,7 +24,7 @@ describe('NewGamePageComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    it('should load games on init', async () => {
+    it('should load games on ngOnInit', async () => {
         const gamesMock: Game[] = [
             {
                 id: '1',
@@ -41,5 +41,39 @@ describe('NewGamePageComponent', () => {
         expect(gameService.getGames).toHaveBeenCalled();
         await fixture.whenStable();
         expect(component.games).toEqual(gamesMock);
+    });
+
+    it('should change the bool of the gameSelected array when function getInformations() is called', async () => {
+        const gamesMock: Game[] = [
+            {
+                id: 'alloBonjour',
+                title: 'game1',
+                description: 'description1',
+                isVisible: true,
+                lastModification: new Date(),
+                duration: 10,
+                questions: [],
+            },
+        ];
+        const gameSelectedMock = { alloBonjour: true };
+        component.gameSelected = gameSelectedMock;
+        component.getInformations(gamesMock[0]);
+        expect(gameSelectedMock['alloBonjour']).toEqual(false);
+    });
+
+    it('should return the array', async () => {
+        const gamesMock: Game[] = [
+            {
+                id: '1',
+                title: 'game1',
+                description: 'description1',
+                isVisible: true,
+                lastModification: new Date(),
+                duration: 10,
+                questions: [],
+            },
+        ];
+        component.games = gamesMock;
+        expect(component.arrayGames).toEqual(gamesMock);
     });
 });
