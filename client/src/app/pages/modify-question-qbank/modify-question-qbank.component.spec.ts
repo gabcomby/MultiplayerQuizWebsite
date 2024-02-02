ef64bd0323344d5fbc05dba6741363875901d81a
Maxsavy <maxime.savy@polymtl.ca>
2024-02-02 13:12:07 -0500

    [FEATURE|MA] improvement modify page question, UI working

--- client/src/app/pages/modify-question-qbank/modify-question-qbank.component.spec.ts
+++ client/src/app/pages/modify-question-qbank/modify-question-qbank.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ModifyQuestionQbankComponent } from './modify-question-qbank.component';
 
 describe('ModifyQuestionQbankComponent', () => {
-  let component: ModifyQuestionQbankComponent;
-  let fixture: ComponentFixture<ModifyQuestionQbankComponent>;
+    let component: ModifyQuestionQbankComponent;
+    let fixture: ComponentFixture<ModifyQuestionQbankComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ModifyQuestionQbankComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ModifyQuestionQbankComponent],
+        });
+        fixture = TestBed.createComponent(ModifyQuestionQbankComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(ModifyQuestionQbankComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
