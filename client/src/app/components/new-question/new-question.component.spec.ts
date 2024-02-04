2a38cac3a71fbe69a09ee2c95ce65448ac045d4f
Alexane123 <112894037+Alexane123@users.noreply.github.com>
2024-02-04 16:55:00 -0500

    [test]AD test new question component

--- client/src/app/components/new-question/new-question.component.spec.ts
+++ client/src/app/components/new-question/new-question.component.spec.ts
@@ -71,15 +71,25 @@ describe('NewQuestionComponent', () => {
         expect(questionServiceSpy.addQuestion).toHaveBeenCalled();
         // the other function
     });
-    // it('should call addQuestion when selecting question from bank and register', () => {
-    //     const newChoices = [
-    //         { text: '1', isValid: false },
-    //         { text: '2', isValid: true },
-    //     ];
-    //     component.question = { type: 'QCM', text: 'allo', points: 10, id: '12312312', lastModification: new Date() };
-    //     const mockOnlyAddQuestionBank = false;
-    //     component.addQuestion(newChoices, mockOnlyAddQuestionBank);
-    //     expect(questionServiceSpy.addQuestion).toHaveBeenCalled();
-    //     // the other function
-    // });
+    it('should call addQuestion for every question selected when calling addQuestionFromBank', () => {
+        const questionFromBank = [
+            {
+                type: 'QCM',
+                text: 'Ceci est une question de test',
+                points: 10,
+                id: 'dsdsd',
+                lastModification: new Date(),
+            },
+            {
+                type: 'QCM',
+                text: 'question 2',
+                points: 10,
+                id: 'alala',
+                lastModification: new Date(),
+            },
+        ];
+        component.addQuestionFromBank(questionFromBank);
+        expect(questionServiceSpy.addQuestion).toHaveBeenCalledWith(questionFromBank[0]);
+        expect(questionServiceSpy.addQuestion).toHaveBeenCalledWith(questionFromBank[1]);
+    });
 });
