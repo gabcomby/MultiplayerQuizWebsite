0607851dbf7105b46e686fee92e3b5f07967cd72
judaoc <112410161+judaoc@users.noreply.github.com>
2024-02-05 21:52:53 -0500

    [FEATURE] |JD| change the button testGame to go to the good place

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -4,7 +4,6 @@ import { authGuard } from '@app/guards/auth.guard';
 import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
 import { CreateQGamePageComponent } from '@app/pages/create-qgame-page/create-qgame-page.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
-import { GameTestComponent } from '@app/pages/game-test/game-test.component';
 import { GameWaitComponent } from '@app/pages/game-wait/game-wait.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
@@ -25,7 +24,7 @@ const routes: Routes = [
     { path: 'create-qgame', component: CreateQGamePageComponent },
     { path: 'create-qgame/:id', component: CreateQGamePageComponent },
     { path: 'question-bank', component: QuestionBankComponent },
-    { path: 'testGame/:id', component: GameTestComponent },
+    { path: 'game/:id', component: GamePageComponent },
     { path: 'gameWait/:id', component: GameWaitComponent },
     { path: 'new-question-qbank', component: NewQuestionQbankComponent },
     { path: 'modify-question-qbank', component: ModifyQuestionQbankComponent },
