3556878f0660287957871dfe01eea41ec25ae0cc
chapsag <chapsag@gmail.com>
2024-02-05 21:42:23 -0500

    [FEATURE | PE] Code barebone of snackbar architecture

--- client/src/app/services/snackbar.service.ts
+++ client/src/app/services/snackbar.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+const DURATION = 3000;
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class SnackbarService {
+    constructor(private snackBar: MatSnackBar) {}
 
-  constructor() { }
+    openSnackBar(message: string, action: string = 'Close', duration: number = DURATION) {
+        this.snackBar.open(message, action, {
+            duration,
+        });
+    }
 }
