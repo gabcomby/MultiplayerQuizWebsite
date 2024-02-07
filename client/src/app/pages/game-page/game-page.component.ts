8a858282222918613ec5aa7f4515d86223bf23b1
Gabriel Comby <combygabriel@gmail.com>
2024-02-07 18:57:41 -0500

    [FEATURE|GC] Socket timer now working however time goes into negative and doesn't reset

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -62,57 +62,139 @@ export class GamePageComponent implements OnInit {
         });
     }
 
-    ngOnInit() {
-        this.route.params.subscribe((params) => {
-            this.gameId = params['id'];
+    async ngOnInit() {
+        // Extract game ID from route parameters first. This doesn't need to be awaited.
+        this.gameId = this.route.snapshot.params['id'];
+
+        // Fetch game data synchronously before setting up WebSocket and other dependencies.
+        await this.fetchGameData(this.gameId);
+
+        // After game data is fetched, proceed with match creation and player addition.
+        await this.createAndSetupMatch();
+
+        // Now that the match is set up and we have the necessary game data,
+        // connect to the WebSocket and set up real-time event listeners.
+        this.setupWebSocketEvents();
+
+        // Finally, start the WebSocket timer now that everything is set up.
+        this.socketService.startTimer();
+    }
+
+    // Convert fetchGameData to return a Promise.
+    async fetchGameData(gameId: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.gameService.getGame(gameId).subscribe({
+                next: (gameData: Game) => {
+                    this.gameData = gameData;
+                    resolve();
+                },
+                error: (error) => {
+                    alert(error.message);
+                    reject(error);
+                },
+            });
         });
-        this.fetchGameData(this.gameId);
+    }
+
+    // Create and setup match as a promise to ensure sequential execution.
+    async createAndSetupMatch(): Promise<void> {
         this.matchId = crypto.randomUUID();
-        this.matchService.createNewMatch({ id: this.matchId, playerList: [] }).subscribe({
-            next: (data) => {
-                this.currentMatch = data;
-            },
-            error: (error) => {
-                alert(error.message);
-            },
+        // Wrap the entire operation in a Promise to handle both match creation and player addition.
+        return new Promise((resolve, reject) => {
+            this.matchService.createNewMatch({ id: this.matchId, playerList: [] }).subscribe({
+                next: (matchData) => {
+                    this.currentMatch = matchData;
+                    // After match creation, add player.
+                    this.matchService.addPlayer({ id: 'playertest', name: 'Player 1', score: 0 }, this.matchId).subscribe({
+                        next: (data) => {
+                            this.currentMatch = data;
+                            resolve();
+                        },
+                        error: (error) => {
+                            alert(error.message);
+                            reject(error);
+                        },
+                    });
+                },
+                error: (error) => {
+                    alert(error.message);
+                    reject(error);
+                },
+            });
         });
-        this.matchService.addPlayer({ id: 'playertest', name: 'Player 1', score: 0 }, this.matchId).subscribe({
-            next: (data) => {
-                this.currentMatch = data;
-            },
-            error: (error) => {
-                alert(error.message);
-            },
-        });
+    }
+
+    // Setup WebSocket events without awaiting them since they are real-time and continuous.
+    setupWebSocketEvents() {
         this.socketService.connect();
+        this.socketService.onTimerCountdown((data) => {
+            this.timerCountdown = data;
+        });
+        // Ensure game data duration is available before setting timer duration.
+        if (this.gameData && this.gameData.duration) {
+            this.socketService.setTimerDuration(this.gameData.duration);
+        }
+        // Additional WebSocket event listeners.
         this.socketService.onTimerDuration((data) => {
             // eslint-disable-next-line no-console
             console.log(data);
         });
-
         this.socketService.onTimerUpdate((data) => {
             // eslint-disable-next-line no-console
             console.log(data);
         });
-
-        this.socketService.onTimerCountdown((data) => {
-            this.timerCountdown = data;
-        });
-        this.socketService.setTimerDuration(this.gameData.duration);
-        this.socketService.startTimer();
     }
 
-    fetchGameData(gameId: string): void {
-        this.gameService.getGame(gameId).subscribe({
-            next: (gameData: Game) => {
-                this.gameData = gameData;
-                this.startQuestionTimer();
-            },
-            error: (error) => {
-                alert(error.message);
-            },
-        });
-    }
+    // async ngOnInit() {
+    //     this.socketService.connect();
+    //     await this.socketService.onTimerCountdown((data) => {
+    //         this.timerCountdown = data;
+    //     });
+    //     this.socketService.setTimerDuration(this.gameData.duration);
+    //     this.route.params.subscribe((params) => {
+    //         this.gameId = params['id'];
+    //     });
+    //     this.fetchGameData(this.gameId);
+    //     this.matchId = crypto.randomUUID();
+    //     this.matchService.createNewMatch({ id: this.matchId, playerList: [] }).subscribe({
+    //         next: (data) => {
+    //             this.currentMatch = data;
+    //         },
+    //         error: (error) => {
+    //             alert(error.message);
+    //         },
+    //     });
+    //     this.matchService.addPlayer({ id: 'playertest', name: 'Player 1', score: 0 }, this.matchId).subscribe({
+    //         next: (data) => {
+    //             this.currentMatch = data;
+    //         },
+    //         error: (error) => {
+    //             alert(error.message);
+    //         },
+    //     });
+    //     this.socketService.onTimerDuration((data) => {
+    //         // eslint-disable-next-line no-console
+    //         console.log(data);
+    //     });
+
+    //     this.socketService.onTimerUpdate((data) => {
+    //         // eslint-disable-next-line no-console
+    //         console.log(data);
+    //     });
+    //     this.socketService.startTimer();
+    // }
+
+    // async fetchGameData(gameId: string): void {
+    //     this.gameService.getGame(gameId).subscribe({
+    //         next: (gameData: Game) => {
+    //             this.gameData = gameData;
+    //             this.startQuestionTimer();
+    //         },
+    //         error: (error) => {
+    //             alert(error.message);
+    //         },
+    //     });
+    // }
 
     startQuestionTimer() {
         this.timerService.startTimer(this.questionTimer).subscribe({
