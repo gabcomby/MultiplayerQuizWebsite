04c6533f23aef68eba6c4f10fbf3aa1902e0f93c
Gabriel Comby <combygabriel@gmail.com>
2024-02-06 21:36:42 -0500

    [FEATURE|GC] Started working on socket.io version of game timer

--- server/app/server.ts
+++ server/app/server.ts
@@ -4,14 +4,22 @@ import { AddressInfo } from 'net';
 import { Server as SocketIoServer } from 'socket.io';
 import { Service } from 'typedi';
 
+const ONE_SECOND_IN_MS = 1000;
+
 @Service()
 export class Server {
     private static readonly appPort: string | number | boolean = Server.normalizePort(process.env.PORT || '3000');
     // eslint-disable-next-line @typescript-eslint/no-magic-numbers
     private static readonly baseDix: number = 10;
+    room = {
+        roomId: 'testroom',
+        duration: 0,
+        timerId: 0,
+        currentTime: 0,
+        isRunning: false,
+    };
     private server: http.Server;
     private io: SocketIoServer;
-
     constructor(private readonly application: Application) {}
 
     private static normalizePort(val: number | string): number | string | boolean {
@@ -39,13 +47,50 @@ export class Server {
                 // eslint-disable-next-line no-console
                 console.log('Client disconnected');
             });
+
+            socket.on('set-timer-duration', (duration) => {
+                if (!isNaN(duration)) {
+                    duration = parseInt(duration, 10);
+                    this.room.duration = duration;
+                    // eslint-disable-next-line no-console
+                    console.log('Set duration of time to', duration);
+                    this.io.emit('timer-duration', duration);
+                } else {
+                    this.io.emit('timer-duration', 'Invalid duration');
+                }
+            });
+
+            socket.on('start-timer', () => {
+                this.room.isRunning = true;
+                startCountdownTimer(this.room.duration);
+                this.io.emit('timer-update', 'Timer started');
+            });
+
+            socket.on('stop-timer', () => {
+                clearInterval(this.room.timerId);
+                this.room.isRunning = false;
+                this.room.currentTime = 0;
+                this.io.emit('timer-update', 'Timer stopped');
+            });
+
+            const startCountdownTimer = (duration: number): void => {
+                if (duration > 0) {
+                    const timerId = setInterval(
+                        () => {
+                            this.io.emit('timer-countdown', duration);
+                            duration -= 1;
+                            this.room.currentTime = duration;
+                        },
+                        ONE_SECOND_IN_MS,
+                        duration,
+                    );
+                    this.room.timerId = timerId;
+                } else {
+                    this.io.emit('timer-update', 'Invalid duration');
+                }
+            };
         });
-
-        this.server.listen(Server.appPort);
-        this.server.on('error', (error: NodeJS.ErrnoException) => this.onError(error));
-        this.server.on('listening', () => this.onListening());
     }
-
     private onError(error: NodeJS.ErrnoException): void {
         if (error.syscall !== 'listen') {
             throw error;
