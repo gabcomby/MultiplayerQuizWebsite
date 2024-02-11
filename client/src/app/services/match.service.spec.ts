916caba934777129029e39f2f448cf41b6ff12b8
Gabriel Comby <combygabriel@gmail.com>
2024-02-10 23:21:48 -0500

    [TESTING|GC] Created test file for match.service.ts and added first test

--- client/src/app/services/match.service.spec.ts
+++ client/src/app/services/match.service.spec.ts
@@ -1,16 +1,39 @@
-// import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import type { Match } from '@app/interfaces/match';
+import { MatchService } from '@app/services/match.service';
 
-// import { MatchService } from './match.service';
+describe('MatchService', () => {
+    let service: MatchService;
+    let httpMock: HttpTestingController;
+    const apiUrl = 'http://localhost:3000/api/matches';
 
-// describe('MatchService', () => {
-//     let service: MatchService;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [MatchService],
+        });
+        service = TestBed.inject(MatchService);
+        httpMock = TestBed.inject(HttpTestingController);
+    });
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({});
-//         service = TestBed.inject(MatchService);
-//     });
+    afterEach(() => {
+        httpMock.verify();
+    });
 
-//     it('should be created', () => {
-//         expect(service).toBeTruthy();
-//     });
-// });
+    it('should retrieve all matches', () => {
+        const dummyMatches: Match[] = [
+            { id: '1', playerList: [] },
+            { id: '2', playerList: [] },
+        ];
+
+        service.getAllMatches().subscribe((matches) => {
+            expect(matches.length).toBe(2);
+            expect(matches).toEqual(dummyMatches);
+        });
+
+        const req = httpMock.expectOne(`${apiUrl}/`);
+        expect(req.request.method).toBe('GET');
+        req.flush(dummyMatches);
+    });
+});
