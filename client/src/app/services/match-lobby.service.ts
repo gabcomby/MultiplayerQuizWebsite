dab5933c59eb7985affc66eac2b14a5c7c14b46b
Gabriel Comby <combygabriel@gmail.com>
2024-02-26 15:46:53 -0500

    [BUGFIX|GC] Changed hard coded server URL in match-lobby.service

--- client/src/app/services/match-lobby.service.ts
+++ client/src/app/services/match-lobby.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { API_BASE_URL } from '@app/app.module';
 import { Player } from '@app/interfaces/match';
 import { MatchLobby } from '@app/interfaces/match-lobby';
 import { generateLobbyId, generateNewId } from '@app/utils/assign-new-game-attributes';
@@ -10,8 +11,14 @@ import { Observable } from 'rxjs';
 })
 export class MatchLobbyService {
     // TODO: Change this to the actual server URL
-    private apiUrl = 'http://localhost:3000/api/lobbies';
-    constructor(private http: HttpClient) {}
+    // private apiUrl = 'http://localhost:3000/api/lobbies';
+    private apiUrl: string;
+    constructor(
+        private http: HttpClient,
+        @Inject(API_BASE_URL) apiBaseURL: string,
+    ) {
+        this.apiUrl = `${apiBaseURL}/api/lobbies`;
+    }
 
     getAllLobbies() {
         return this.http.get(`${this.apiUrl}/`);
