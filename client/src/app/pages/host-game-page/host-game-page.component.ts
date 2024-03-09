d1d90a4c3aedcaeadd78cd19859fb48a5cabf046
Maxsavy <maxime.savy@polymtl.ca>
2024-03-08 22:16:33 -0500

    added needed services with init and destroy

--- client/src/app/pages/host-game-page/host-game-page.component.ts
+++ client/src/app/pages/host-game-page/host-game-page.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { MatchLobby } from '@app/interfaces/match-lobby';
+import { GameService } from '@app/services/game.service';
+import { MatchLobbyService } from '@app/services/match-lobby.service';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-host-game-page',
     templateUrl: './host-game-page.component.html',
     styleUrls: ['./host-game-page.component.scss'],
 })
-export class HostGamePageComponent {
+export class HostGamePageComponent implements OnInit, OnDestroy {
     endGame = false;
     lobby: MatchLobby;
+    unsubscribeSubject: Subscription[];
+    private destroy = new Subject<void>();
+    constructor(
+        private route: ActivatedRoute,
+        private matchLobbyService: MatchLobbyService,
+        private gameService: GameService,
+    ) {}
+
+    ngOnInit(): void {
+        return;
+    }
+    ngOnDestroy() {
+        this.destroy.next();
+        this.destroy.complete();
+        this.unsubscribeSubject.forEach((subject) => {
+            subject.unsubscribe();
+        });
+    }
 }
