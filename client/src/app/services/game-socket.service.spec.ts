6f39a7a1cb6c1d82b20cc9ef9352a48167eff42d
chapsag <goffi.pro@icloud.com>
2024-03-09 14:56:53 -0500

    [TESTING | PE] Ajouter des imports et des fournisseurs pour les tests de GameSocketService

--- client/src/app/services/game-socket.service.spec.ts
+++ client/src/app/services/game-socket.service.spec.ts
@@ -1,16 +1,21 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { API_BASE_URL } from '@app/app.module';
 import { GameSocketService } from './game-socket.service';
 
 describe('GameSocketService', () => {
-  let service: GameSocketService;
+    let service: GameSocketService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GameSocketService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [GameSocketService, { provide: API_BASE_URL, useValue: 'http://localhost:3000/api' }],
+            imports: [HttpClientTestingModule, MatSnackBarModule],
+        });
+        service = TestBed.inject(GameSocketService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
