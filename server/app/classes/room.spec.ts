9ec41cccdeadee3855688f691691f595df90b0ee
Maxsavy <maxime.savy@polymtl.ca>
2024-03-18 00:39:03 -0400

    [TESTING|MA] ALMOST finished room tests, don't understand the weird problem

--- server/app/classes/room.spec.ts
+++ server/app/classes/room.spec.ts
@@ -1,6 +1,5 @@
-/* eslint-disable max-lines */
-// it is a test file so it is normal to have a lot of lines
-import { Room } from '@app/classes/room'; // Update the import path according to your project structure
+/* eslint-disable max-lines  -- it is a test file so it is normal to have a lot of lines */
+import { Room } from '@app/classes/room';
 import gameModel from '@app/model/game.model';
 import { IPlayer } from '@app/model/match.model';
 import * as chai from 'chai';
@@ -11,8 +10,7 @@ import * as SocketIO from 'socket.io';
 const ID_LOBBY_LENGTH = 4;
 const FIRST_ANSWER_MULTIPLIER = 1.2;
 const TIME_BETWEEN_QUESTIONS_TEST_MODE = 5000;
-// const ONE_SECOND_IN_MS = 1000;
-// const TIMER_QUESTION_DURATION = 5;
+const ONE_SECOND_IN_MS = 1000;
 
 const mockGame = new gameModel({
     id: '1a2b3c',
@@ -85,7 +83,6 @@ class MockSocketIO {
     to(roomId: string) {
         if (!this.rooms[roomId]) {
             this.rooms[roomId] = new MockSocketIO();
-            // Transfer callbacks to the room-specific instance to simulate room-specific behavior
             this.rooms[roomId].callbacks = { ...this.callbacks };
         }
         return {
@@ -188,12 +185,12 @@ describe('Room', () => {
         room.isTestRoom = true;
         room.isRunning = false;
         room.launchTimer = false;
-        room.currentQuestionIndex = 0; // Ensure not the last question
+        room.currentQuestionIndex = 0;
 
         room.handleTimerEnd();
 
         clock.tick(TIME_BETWEEN_QUESTIONS_TEST_MODE);
-        assert.equal(room.currentQuestionIndex, 1); // Assuming startQuestion increases index
+        assert.equal(room.currentQuestionIndex, 1);
     });
     it('should handle answers for repeated answers', () => {
         const existingPlayerId = 'existingPlayerId';
@@ -227,14 +224,15 @@ describe('Room', () => {
         assert.isFalse(room.isRunning);
     });
 
-    // it('should emit "go-to-results" when the last question is answered', () => {
-    //     room.isRunning = false;
-    //     room.currentQuestionIndex = room.game.questions.length - 1;
+    it('should emit "go-to-results" when the last question is answered', () => {
+        room.isRunning = false;
+        room.launchTimer = false;
+        room.currentQuestionIndex = room.game.questions.length - 1;
 
-    //     room.startQuestion();
+        room.startQuestion();
 
-    //     sinon.assert.calledWithMatch(mockSocketIoServer.emit, 'go-to-results', sinon.match.any, sinon.match.any, sinon.match.any);
-    // });
+        sinon.assert.calledWithMatch(mockSocketIoServer.emit, 'go-to-results', sinon.match.any, sinon.match.any, sinon.match.any);
+    });
 
     it('should reset playerHasAnswered to false for all players at the start of a new question', () => {
         const player1: IPlayer = { id: 'player1Id', score: 0, bonus: 0, name: 'testPlayer' } as IPlayer;
@@ -243,14 +241,12 @@ describe('Room', () => {
         room.playerList.set('player2Id', player2);
         room.playerHasAnswered.set('player1', true);
         room.playerHasAnswered.set('player2', true);
-        room.isRunning = false; // Ensure the room is not already running
-        room.launchTimer = false; // Assume launch timer has completed
-        room.currentQuestionIndex = 0; // Simulate starting at the first question
+        room.isRunning = false;
+        room.launchTimer = false;
+        room.currentQuestionIndex = 0;
 
-        // Trigger the method under test
         room.startQuestion();
 
-        // Assertions
         assert.isFalse(room.playerHasAnswered.get('player1'), 'Player 1 should be reset to not have answered');
         assert.isFalse(room.playerHasAnswered.get('player2'), 'Player 2 should be reset to not have answered');
     });
@@ -261,39 +257,18 @@ describe('Room', () => {
         const player2: IPlayer = { id: 'player2Id', score: 0, bonus: 0, name: 'testPlayer' } as IPlayer;
         room.playerList.set('player2Id', player2);
 
-        // Simulate players having submitted answers for a previous question
         room.livePlayerAnswers.set('player1Id', [1, 2]);
         room.livePlayerAnswers.set('player2Id', [2, 3]);
 
-        room.isRunning = false; // Ensure the room is not already running
-        room.launchTimer = false; // Assume launch timer has completed
-        room.currentQuestionIndex = 0; // Simulate starting at the first question
+        room.isRunning = false;
+        room.launchTimer = false;
+        room.currentQuestionIndex = 0;
 
-        // Trigger the method under test
         room.startQuestion();
 
-        // Assertions
         assert.deepEqual(room.livePlayerAnswers.get('player1Id'), [], "Player 1's livePlayerAnswers should be reset to an empty array");
         assert.deepEqual(room.livePlayerAnswers.get('player2Id'), [], "Player 2's livePlayerAnswers should be reset to an empty array");
     });
-
-    // it('should emit "timer-countdown" with the initial duration and start the countdown', () => {
-    //     room.duration = 5; // Set initial countdown duration
-    //     room.startCountdownTimer(); // Start the countdown timer
-
-    //     // The first emit call should be with the initial duration
-    //     sinon.assert.calledWith(mockSocketIoServer.emit.firstCall, 'timer-countdown', TIMER_QUESTION_DURATION);
-
-    //     // Advance time to simulate each second of the countdown, and verify the expected "timer-countdown" emissions
-    //     for (let secondsLeft = 4; secondsLeft >= 1; secondsLeft--) {
-    //         clock.tick(ONE_SECOND_IN_MS); // Advance time by one second
-    //         sinon.assert.calledWith(mockSocketIoServer.emit, 'timer-countdown', secondsLeft);
-    //     }
-
-    //     // Advance time for the final second and expect "timer-stopped"
-    //     clock.tick(ONE_SECOND_IN_MS);
-    //     sinon.assert.calledWith(mockSocketIoServer.emit, 'timer-stopped');
-    // });
 
     it('should not update score or bonus for a player who submits no answer (empty array)', () => {
         const playerId = 'playerWithMultipleAnswers';
@@ -302,14 +277,11 @@ describe('Room', () => {
         const initialScore = player.score;
         const initialBonus = player.bonus;
 
-        // Act: Simulate the player submitting no answer
         room.verifyAnswers(player.id, []);
 
-        // Assert: Check that the player's score and bonus have not changed
         assert.equal(room.playerList.get(player.id).score, initialScore, 'Player score should not change');
         assert.equal(room.playerList.get(player.id).bonus, initialBonus, 'Player bonus should not change');
 
-        // Additionally, you might want to assert that the player is now marked as having answered
         assert.isTrue(room.playerHasAnswered.get(player.id), 'Player should be marked as having answered');
     });
 
@@ -319,22 +291,17 @@ describe('Room', () => {
         const player: IPlayer = { id: playerId, score: 0, bonus: 0, name: 'testPlayer' } as IPlayer;
         room.playerList.set(player.id, player);
 
-        // Reset playerHasAnswered and livePlayerAnswers for a clean state
         room.playerHasAnswered.clear();
         room.livePlayerAnswers.clear();
 
-        // Set initial conditions for a question with multiple answers where at least one is incorrect
-        const correctAnswerIndexes = [0]; // Assuming index 0 is a correct answer
-        const incorrectAnswerIndexes = [1]; // Assuming index 1 is an incorrect answer
+        const correctAnswerIndexes = [0];
+        const incorrectAnswerIndexes = [1];
         const submittedAnswerIndexes = correctAnswerIndexes.concat(incorrectAnswerIndexes);
 
-        // Act: Simulate the player submitting both correct and incorrect answers
         room.verifyAnswers(player.id, submittedAnswerIndexes);
 
-        // Assert: Check that the player's score did not increase
         assert.equal(room.playerList.get(player.id).score, 0, 'Player score should not change due to incorrect submission');
 
-        // Additionally, assert that the player is marked as having answered
         assert.isTrue(room.playerHasAnswered.get(player.id), 'Player should be marked as having answered');
     });
 
@@ -344,9 +311,8 @@ describe('Room', () => {
         const player: IPlayer = { id: playerId, score: 0, bonus: 0, name: 'testPlayer' } as IPlayer;
         room.playerList.set(player.id, player);
 
-        // Assume the player hasn't answered yet for this question
         room.playerHasAnswered.set(player.id, false);
-        const answerIdx = [0]; // Correct answer index for the first question
+        const answerIdx = [0];
         const initialScore = player.score;
 
         room.firstAnswerForBonus = false;
@@ -360,15 +326,76 @@ describe('Room', () => {
         );
     });
 
-    // it('should process early answers and emit "timer-stopped" when all players have answered', () => {
-    //     room.isRunning = true;
-    //     const player1: IPlayer = { id: 'player1Id', score: 0, bonus: 0, name: 'testPlayer' } as IPlayer;
-    //     room.playerList.set('player1Id', player1);
-    //     room.currentQuestionIndex = 0;
-    //     room.lockedAnswers = 0;
+    it('should handle countdown timer correctly, emitting events and stopping at zero', () => {
+        const duration = 5;
+        room.duration = duration;
+        room.roomId = 'testRoomId';
+        room.firstAnswerForBonus = true;
+        room.launchTimer = false;
+        const handleTimerEndSpy = sinon.spy(room, 'handleTimerEnd');
 
-    //     room.handleEarlyAnswers(player1.id, [0]);
+        room.startCountdownTimer();
 
-    //     assert.equal(room.lockedAnswers, 1, 'lockedAnswers should be incremented');
-    // });
+        for (let currentTime = duration - 1; currentTime >= 0; currentTime--) {
+            clock.tick(ONE_SECOND_IN_MS);
+            sinon.assert.calledWith(mockSocketIoServer.emit, 'timer-countdown', currentTime);
+        }
+
+        sinon.assert.calledWith(mockSocketIoServer.emit, 'timer-stopped');
+        sinon.assert.calledOnce(handleTimerEndSpy);
+    });
+
+    it('should handle early answers correctly, emitting "timer-stopped" when all players have answered', () => {
+        const player1Id = 'player1Id';
+        const player2Id = 'player2Id';
+        const player1 = { id: player1Id, score: 0, bonus: 0, name: 'Player 1' } as IPlayer;
+        const player2 = { id: player2Id, score: 0, bonus: 0, name: 'Player 2' } as IPlayer;
+        const player3 = { id: 'player3Id', score: 0, bonus: 0, name: 'Player 3' } as IPlayer;
+        room.playerList.set(player1Id, player1);
+        room.playerList.set(player2Id, player2);
+        room.playerList.set('player3Id', player3);
+        room.roomId = 'testRoomId';
+        room.isRunning = true;
+
+        const verifyAnswersStub = sinon.stub(room, 'verifyAnswers');
+
+        room.handleEarlyAnswers('player1Id', [0]);
+        room.handleEarlyAnswers('player2Id', [1]);
+
+        assert.equal(room.lockedAnswers, 2, 'All players have locked in answers');
+
+        room.handleEarlyAnswers('player3Id', [1]);
+        sinon.assert.calledWith(mockSocketIoServer.emit, 'timer-stopped');
+
+        verifyAnswersStub.restore();
+    });
+
+    it('should not emit "playerlist-change" when not all players have answered', () => {
+        room.playerList.set('player1', { id: 'player1', score: 0, bonus: 0, name: 'Player 1' } as IPlayer);
+        room.playerList.set('player2', { id: 'player2', score: 0, bonus: 0, name: 'Player 2' } as IPlayer);
+        room.currentQuestionIndex = 0;
+
+        room.verifyAnswers('player1', [0]);
+
+        sinon.assert.neverCalledWith(mockSocketIoServer.emit, 'playerlist-change');
+    });
+
+    it('should not emit "timer-stopped" immediately if countdown is active, despite launchTimer being false', () => {
+        room.duration = 5;
+        room.launchTimer = false;
+        room.isRunning = true;
+
+        room.startCountdownTimer();
+
+        for (let currentTime = room.duration - 1; currentTime > 0; currentTime--) {
+            clock.tick(ONE_SECOND_IN_MS);
+            sinon.assert.calledWith(mockSocketIoServer.emit, 'timer-countdown', currentTime);
+        }
+
+        sinon.assert.neverCalledWith(mockSocketIoServer.emit, 'timer-stopped');
+
+        clock.tick(ONE_SECOND_IN_MS);
+
+        sinon.assert.calledWith(mockSocketIoServer.emit, 'timer-stopped');
+    });
 });
