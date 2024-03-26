adf76140295dccfdf82b81b721569ca5d03ca02a
Jubergie <julie-2.bergeron@polymtl.ca>
2024-03-25 21:46:04 -0400

    [FEATURE | JB] cuts question text if exceeds 200 characters

--- client/src/app/components/modified-question/modified-question.component.ts
+++ client/src/app/components/modified-question/modified-question.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Question } from '@app/interfaces/game';
 import { QuestionService } from '@app/services/question.service';
 
+const MAX_LENGTH = 200;
+
 @Component({
     selector: 'app-modified-question',
     templateUrl: './modified-question.component.html',
@@ -30,6 +32,10 @@ export class ModifiedQuestionComponent implements OnInit {
             this.questionList.push(question);
             this.disabled.push(true);
         });
+
+        this.questionList.forEach((question) => {
+            this.cutText(question);
+        });
     }
 
     setQuestionList() {
@@ -54,6 +60,7 @@ export class ModifiedQuestionComponent implements OnInit {
     }
 
     saveQuestion(index: number) {
+        this.cutText(this.questionList[index]);
         const validated = this.questionService.saveQuestion(index, this.questionList, this.listQuestionBank);
 
         this.disabled[index] = validated;
@@ -67,5 +74,11 @@ export class ModifiedQuestionComponent implements OnInit {
 
     toggleMenuSelection(): void {
         this.menuSelected = !this.menuSelected;
+    }
+
+    cutText(question: Question): void {
+        if (question.text.length > MAX_LENGTH) {
+            question.text = question.text.substring(0, MAX_LENGTH) + '...';
+        }
     }
 }
