c8251dbf86ff1a234a6dd217fddd8798e01ae3dc
chapsag <goffi.pro@icloud.com>
2024-04-09 08:20:52 -0400

    [FEATURE | PE] Mise en place d'un tableau de scores avec tri pour la page de jeu

--- client/src/app/components/game-page-scoresheet/game-page-scoresheet.component.ts
+++ client/src/app/components/game-page-scoresheet/game-page-scoresheet.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import type { Player } from '@app/interfaces/match';
 
 @Component({
@@ -6,8 +8,22 @@ import type { Player } from '@app/interfaces/match';
     templateUrl: './game-page-scoresheet.component.html',
     styleUrls: ['./game-page-scoresheet.component.scss'],
 })
-export class GamePageScoresheetComponent {
+export class GamePageScoresheetComponent implements OnInit, OnChanges {
     @Input() playerList: Player[];
     @Input() playerLeftList: Player[];
     @Input() isHost: boolean;
+
+    @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+    dataSource: MatTableDataSource<Player> = new MatTableDataSource();
+
+    ngOnInit() {
+        this.dataSource.sort = this.sort;
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes['playerList']) {
+            this.dataSource.data = this.playerList;
+        }
+    }
 }
