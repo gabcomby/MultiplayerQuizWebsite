6934d9d62f262b28a303d6a004c7403255209694
judaoc <112410161+judaoc@users.noreply.github.com>
2024-04-09 13:38:46 -0400

    [UX] |JD| the list of game doesn't have holes

--- client/src/app/pages/new-game-page/new-game-page.component.ts
+++ client/src/app/pages/new-game-page/new-game-page.component.ts
@@ -21,7 +21,7 @@ export class NewGamePageComponent implements OnInit {
     games: Game[] = [];
     gameSelected: { [key: string]: boolean } = {};
     socket: Socket;
-
+    gamesVisible: Game[] = [];
     deletedGamesId: string[] = [];
     subscription: Subscription;
 
@@ -36,9 +36,10 @@ export class NewGamePageComponent implements OnInit {
     ) {}
 
     async ngOnInit() {
-        this.apiService.getGames().then((games) => {
+        await this.apiService.getGames().then((games) => {
             this.games = games;
         });
+        this.gamesVisibleList();
     }
 
     selected(game: Game): void {
@@ -141,5 +142,9 @@ export class NewGamePageComponent implements OnInit {
                 }
             },
         });
+    }
+    gamesVisibleList(): void {
+        this.gamesVisible = this.games.filter((game) => game.isVisible);
+        console.log(this.gamesVisible);
     }
 }
