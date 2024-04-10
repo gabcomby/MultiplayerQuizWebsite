edbaa76d6f5ca339917c900c2c78083db7fa46fe
chapsag <goffi.pro@icloud.com>
2024-04-10 13:12:21 -0400

    [FEATURE | PE] Supprimer l'interface IMatch et le modèle de match inutilisés

--- server/app/model/match.model.ts
+++ server/app/model/match.model.ts
@@ -14,22 +14,3 @@ export interface IPlayer extends Document {
     chatPermission?: boolean;
     status?: PlayerStatus;
 }
-
-// export interface IMatch extends Document {
-//     id: string;
-//     playerList: IPlayer[];
-// }
-
-// export const playerSchema: Schema = new Schema({
-//     id: { type: String, required: true },
-//     name: { type: String, required: true },
-//     score: { type: Number, required: true },
-//     bonus: { type: Number, required: true },
-// });
-
-// const matchSchema: Schema = new Schema({
-//     id: { type: String, required: true },
-//     playerList: [playerSchema],
-// });
-
-// export default mongoose.model<IPlayer>('Player', playerSchema);
