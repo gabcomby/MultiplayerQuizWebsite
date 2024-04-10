512b1803507928dcea322bd256c913674db9c54f
chapsag <goffi.pro@icloud.com>
2024-04-10 13:12:03 -0400

    [FEATURE | PE] Ajouter l'énumération PlayerStatus et la propriété de statut pour les joueurs

--- server/app/model/match.model.ts
+++ server/app/model/match.model.ts
@@ -1,11 +1,18 @@
 import { Document } from 'mongoose';
 
+export enum PlayerStatus {
+    Inactive,
+    Active,
+    Confirmed,
+}
+
 export interface IPlayer extends Document {
     id: string;
     name: string;
     score: number;
     bonus: number;
     chatPermission?: boolean;
+    status?: PlayerStatus;
 }
 
 // export interface IMatch extends Document {
