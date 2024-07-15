10f1cfd4b2c51ea8d6dd52a7e6fb3b4f3379d6fd
Gabriel Comby <83326462+gabcomby@users.noreply.github.com>
2024-07-15 15:21:15 -0400

    Revert "Add placeholders for .env files for client & server"

--- client/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    serverUrl: '', // Your AWS EC2 instance URL pointing to /api (ex: 'http://ec2-xx-xxx-xxx-xxx.us-east-2.compute.amazonaws.com:3000/api')
-    socketUrl: '', // Your AWS EC2 instance URL pointing to / (ex: 'http://ec2-xx-xxx-xxx-xxx.us-east-2.compute.amazonaws.com:3000')
-};
--- server/app/env.ts
+++
@@ -1,3 +0,0 @@
-export const env = {
-    dbUrl: '', // Your MongoDB Atlas URL pointing to the database (ex: 'mongodb+srv://<username>:<password>@<cluster>/<database>?retryWrites=true&w=majority')
-};
